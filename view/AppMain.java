241d6b5c429bba7302c3cf23b83f3e9f4eebde49
arahnidx <39658294+arahnidx@users.noreply.github.com>
2022-02-05 19:36:01 -0300

    comment added

--- view/AppMain.java
+++ view/AppMain.java
@@ -7,7 +7,7 @@ public class AppMain {
 	public static void main(String[] args) {
 		String admin = "admin", senhaMestre = "123"; /* Login de admin, para caso n�o haja nenhuma unidade criada e para controle total */
 		Banco Nubank = new Banco(); //Banco padr�o que tem como herdeiros as unidades
-		new Login(admin, senhaMestre, Nubank); 
+		new Login(admin, senhaMestre, Nubank);  //abrir login
 
 	}
 
